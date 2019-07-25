6cb9b69d62dcbe954b0e36546e32fdb59390084f
John Reese <john@noswap.com>
2019-07-24 19:33:06 -0700

    Version bump v0.6.0

--- aiomultiprocess/__init__.py
+++ aiomultiprocess/__init__.py
@@ -6,6 +6,6 @@ AsyncIO version of the standard multiprocessing module
 """
 
 __author__ = "John Reese"
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 from .core import Pool, Process, Worker, set_context
