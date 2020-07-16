01ce689710edcb8c6f0bfabea93300840f258e84
bion howard <bion@bitpharma.com>
2020-07-16 13:48:04 -0400

    add "TEST_WITH_UVLOOP" os flag in main
    this is the simplest way i know to rerun the tests with uvloop

--- aiomultiprocess/tests/__main__.py
+++ aiomultiprocess/tests/__main__.py
@@ -2,6 +2,9 @@
 # Licensed under the MIT license
 
 import unittest
+import os
 
 if __name__ == "__main__":  # pragma: no cover
+    unittest.main(module="aiomultiprocess.tests", verbosity=2)
+    os.environ['TEST_WITH_UVLOOP'] = "OK"
     unittest.main(module="aiomultiprocess.tests", verbosity=2)
